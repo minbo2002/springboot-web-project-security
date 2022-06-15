3726d1c21cea1de05486643fc695d94a9bc6af65
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-06-16 04:56:04 +0900

    1. spring security 프로젝트 생성 및 시큐리티 동작 확인

---
+++ src/main/java/org/zerock/club/SpringbootWebProjectSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebProjectSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebProjectSecurityApplication.class, args);
+    }
+
+}
